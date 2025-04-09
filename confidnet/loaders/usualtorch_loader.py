6b39e303cb923c0851b18d80f532ac8e38ed59be
Trevor Small <180577963+trevors0818@users.noreply.github.com>
2025-04-09 12:20:02 -0400

    Merged master into local repo

--- confidnet/loaders/usualtorch_loader.py
+++ confidnet/loaders/usualtorch_loader.py
@@ -5,80 +5,6 @@ from confidnet.augmentations import get_composed_augmentations
 from confidnet.loaders.camvid_dataset import CamvidDataset
 from confidnet.loaders.loader import AbstractDataLoader
 
-class CityscapesLoader(AbstractDataLoader):
-    def add_augmentations(self):
-        self.augmentations_train = get_composed_augmentations(
-            self.augmentations, training="segmentation"
-        )
-        self.augmentations_val = get_composed_augmentations(
-            {
-                key: self.augmentations[key]
-                for key in self.augmentations
-                if key in ["normalize", "resize"]
-            },
-            verbose=False,
-            training="segmentation",
-        )
-        self.augmentations_test = get_composed_augmentations(
-            {key: self.augmentations[key] for key in self.augmentations if key == "normalize"},
-            verbose=False,
-            training="segmentation",
-        )
-
-    def load_dataset(self):
-        target_type = "semantic"  # Can also be "instance" or a list like ["semantic", "instance"]
-
-        self.train_dataset = Cityscapes(
-            root=self.data_dir,
-            split="train",
-            mode="fine",
-            target_type=target_type,
-            transform=self.augmentations_train,
-            target_transform=self.augmentations_train,
-        )
-
-        self.val_dataset = Cityscapes(
-            root=self.data_dir,
-            split="val",
-            mode="fine",
-            target_type=target_type,
-            transform=self.augmentations_val,
-            target_transform=self.augmentations_val,
-        )
-
-        self.test_dataset = Cityscapes(
-            root=self.data_dir,
-            split="test",
-            mode="fine",
-            target_type=target_type,
-            transform=self.augmentations_test,
-            target_transform=self.augmentations_test,
-        )
-
-    def make_loaders(self):
-        self.train_loader = torch.utils.data.DataLoader(
-            dataset=self.train_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            pin_memory=self.pin_memory,
-            num_workers=self.num_workers,
-        )
-        self.val_loader = torch.utils.data.DataLoader(
-            dataset=self.val_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            pin_memory=self.pin_memory,
-            num_workers=self.num_workers,
-        )
-        self.test_loader = torch.utils.data.DataLoader(
-            dataset=self.test_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            pin_memory=self.pin_memory,
-            num_workers=self.num_workers,
-        )
-
-
 class FashionMNISTLoader(AbstractDataLoader):
     def load_dataset(self):
         self.train_dataset = datasets.FashionMNIST(
@@ -90,11 +16,14 @@ class FashionMNISTLoader(AbstractDataLoader):
 
 class ImagenetteLoader(AbstractDataLoader):
     def load_dataset(self):
-        self.train_dataset = datasets.Imagenette(
-            root=self.data_dir, train=True, download=True, transform=self.augmentations_train
+        train_dir = os.path.join(self.data_dir, "imagenette/train")
+        val_dir = os.path.join(self.data_dir, "imagenette/val")
+
+        self.train_dataset = ImageFolder(
+            root=train_dir, transform=self.augmentations_train
         )
-        self.test_dataset = datasets.Imagenette(
-            root=self.data_dir, train=False, download=True, transform=self.augmentations_test
+        self.test_dataset = ImageFolder(
+            root=val_dir, transform=self.augmentations_test
         )
 
 
